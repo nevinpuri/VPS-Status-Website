7246223b1293401dd168b4e69c03646e2b7051b1
Nevin1901 <nevinpuri1901@gmail.com>
2020-07-11 20:44:51 +0200

    cleaned up code

--- website/src/App.js
+++ website/src/App.js
@@ -1,31 +1,14 @@
-import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { Component, useState, useEffect } from "react";
 import VPSData from "./VPSData.js";
 import "./style.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vpsData: [],
-      loading: true,
-    };
-  }
-
-  componentDidMount = async () => {
-    this.setState({ loading: false });
-  };
-
-  render() {
-    return (
-      !this.state.loading && (
-        <div>
-          <h1 className="text-center jumbotron">Nevin.CC VPS Status</h1>
-          <VPSData vpsStatus={this.state.vpsData} className="text-center" />
-        </div>
-      )
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center jumbotron">Nevin.cc Server Status</h1>
+      <VPSData className="text-center" />
+    </div>
+  );
+};
 
 export default App;
